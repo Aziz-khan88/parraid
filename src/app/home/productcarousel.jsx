c54e0ec73336c534c1fec5f9032c0774bba580cf
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-11-20 16:16:40 +0500

    change the color of font and visibale on products

--- src/app/home/productcarousel.jsx
+++ src/app/home/productcarousel.jsx
@@ -7,64 +7,68 @@ import TwoButton from "@/src/app/components/twobutton";
 import { ServiceArrow } from "@/src/app/app-constants";
 
 const ProductCarousel = ({ products }) => {
+  const [emblaRef, embla] = useEmblaCarousel({
+    align: "center",
+    slidesToScroll: 1,
+    containScroll: "trimSnaps",
+  });
 
-    const [emblaRef, embla] = useEmblaCarousel({
-        align: "center",
-        slidesToScroll: 1,
-        containScroll: "trimSnaps",
-    });
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setCurrentIndex(embla.selectedScrollSnap());
+  }, [embla]);
 
-    const onSelect = useCallback(() => {
-        if (!embla) return;
-        setCurrentIndex(embla.selectedScrollSnap());
-    }, [embla]);
+  useEffect(() => {
+    if (!embla) return;
+    embla.on("select", onSelect);
+    onSelect();
+  }, [embla, onSelect]);
 
-    useEffect(() => {
-        if (!embla) return;
-        embla.on("select", onSelect);
-        onSelect();
-    }, [embla, onSelect]);
+  const prevButtonHandler = () => embla && embla.scrollPrev();
+  const nextButtonHandler = () => embla && embla.scrollNext();
 
-    const prevButtonHandler = () => embla && embla.scrollPrev();
-    const nextButtonHandler = () => embla && embla.scrollNext();
+  return (
+    <section className={styles.embla}>
+      <div className={styles.embla__viewport} ref={emblaRef}>
+        <div className={styles.embla__container}>
+          {products.map((item, index) => (
+            <div className={styles.embla__slide} key={index}>
+              <div className={styles.productCard}>
+                <h5>{item.name}</h5>
 
-    return (
-        <section className={styles.embla}>
-            <div className={styles.embla__viewport} ref={emblaRef}>
-                <div className={styles.embla__container}>
-                    {products.map((item, index) => (
-                        <div className={styles.embla__slide} key={index}>
-                            <div className={styles.productCard}>
-                                <h5>{item.name}</h5>
-                                <div className={styles.contentSec}>
-                                    <p>{item.txt}</p>
-                                    <TwoButton link1={`/product/${item.slug}`} link2="#" black={true} />
-                                </div>
-                                <Image
-                                    src={item.img}
-                                    alt={item.name}
-                                    width={322}
-                                    height={118}
-                                />
-                            </div>
-                        </div>
-                    ))}
+                <p>{item.txt}</p>
+                <div className={styles.contentSec}>
+                  <TwoButton
+                    link1={`/product/${item.slug}`}
+                    link2="#"
+                    black={true}
+                  />
                 </div>
+                <Image
+                  src={item.img}
+                  alt={item.name}
+                  width={322}
+                  height={118}
+                />
+              </div>
             </div>
-            {products.length > 4 && (
-                <div className="embla_container">
-                    <div className="embla_prev" onClick={prevButtonHandler}>
-                        <ServiceArrow direction="prev" />
-                    </div>
-                    <div className="embla_next" onClick={nextButtonHandler}>
-                        <ServiceArrow direction="next" />
-                    </div>
-                </div>
-            )}
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+      {products.length > 4 && (
+        <div className="embla_container">
+          <div className="embla_prev" onClick={prevButtonHandler}>
+            <ServiceArrow direction="prev" />
+          </div>
+          <div className="embla_next" onClick={nextButtonHandler}>
+            <ServiceArrow direction="next" />
+          </div>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default ProductCarousel;

055025e9c71a3e0cc758ad84cee6aded6677244f
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-12-08 21:39:22 +0500

    events data uploaded

--- src/app/resources/components/listing.jsx
+++ src/app/resources/components/listing.jsx
@@ -9,19 +9,16 @@ const ListingPage = ({ data }) => {
                 <Row>
                     <Col lg={12}>
                         <div className={styles.listingPageSec}>
-
                             {data.map((item, index) => (
                                 <div className={styles.listingPageItem} key={index}>
                                     <Link href={item.slug}>{item.title}</Link>
                                 </div>
                             ))}
-
-
                         </div>
                     </Col>
                 </Row>
             </Container>
-        </section >
+        </section>
     )
 }
 
--- src/app/resources/page.jsx
+++ src/app/resources/page.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import Banner from '@/src/app/resources/components/banner'
 import ListingPage from '@/src/app/resources/components/listing'
-import { PageData } from '@/src/app/data/pages/data'
+import { PageData } from '@/src/app/data/pages/data';
 
 const Page = () => {
     return (
